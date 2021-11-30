c4b912299ff4e7b44ed22b24e3a40ea7c23ef946
BrainModes Research <BrainModes@users.noreply.github.com>
2021-11-30 11:07:29 +0100

    uploaded src

---
+++ src/run_pipeline.sh
@@ -0,0 +1,4 @@
+./tvb_image_processing_pipeline_v4.sh -m 1 -p . -c /scratch/snx3000/bp000225/containerstore
+./tvb_image_processing_pipeline_v4.sh -m 11 -p /scratch/snx3000/bp000225/working_dir -c /scratch/snx3000/bp000225/containerstore
+./tvb_image_processing_pipeline_v4.sh -m 12 -p /scratch/snx3000/bp000225/working_dir -c /scratch/snx3000/bp000225/containerstore
+
